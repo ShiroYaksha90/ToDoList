849b3cd5a576c93060f8b15858942a96a7efc2e7
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2022-12-22 21:18:05 +0300

    Add modules to split code files Update index.js Update index.html Update style.css

--- src/index.js
+++ src/index.js
@@ -1,33 +1,101 @@
 import './style.css';
+import { addToLocalStorage, removeFromLocalStorage, editLocalStorage } from './modules/storage.js';
+import resetIndex from './modules/update.js';
 
-const data = [
-  {
-    desc: 'Go shopping',
-    completed: false,
-    index: 1,
-  },
-  {
-    desc: 'Take out the trash',
-    completed: false,
-    index: 2,
-  },
-  {
-    desc: "Finish today's tasks",
-    completed: false,
-    index: 3,
-  },
-];
-const listContainer = document.querySelector('.itemesList');
+const listContainer = document.querySelector('.list-container');
+const myForm = document.querySelector('.form');
+const input = document.querySelector('.to-do');
+const itemesList = document.querySelector('.itemesList');
+const clearbtn = document.querySelector('.clearbtn');
 
-window.addEventListener('DOMContentLoaded', () => {
-  const listString = data.map((list) => `<article>
-          <div class="item">
-              <input type="checkbox">
-              <p>${list.desc}</p>
-          </div>
-          <i class="fa-solid fa-ellipsis-vertical"></i>
-      </article>`)
-    .join('');
-    //   console.log(listString)
-  listContainer.innerHTML = listString;
+// Add function
+const addTask = (e) => {
+  e.preventDefault();
+  const tasksList = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+  const item = input.value;
+  const status = false;
+  const index = (tasksList.length + 1).toString();
+  if (item) {
+    const article = document.createElement('article');
+    article.className = 'article';
+    const attribute = document.createAttribute('data-index');
+    attribute.value = index;
+    article.setAttributeNode(attribute);
+    article.innerHTML = `<div class="item">
+    <input type="checkbox"><div class="list"><p class="list-item">${item}</p></div>
+    </div>
+    <div class="icons">
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+    <i class="fa-solid fa-trash-clock fa-trash"></i>
+    </div>`;
+    // const p = document.querySelector('.list-item');
+    const opBtn = article.querySelector('.fa-ellipsis-vertical');
+    const removeBtn = article.querySelector('.fa-trash');
+    opBtn.addEventListener('click', (e) => {
+      opBtn.classList.add('hide');
+      removeBtn.classList.add('show');
+      const editList = e.target.parentElement.previousElementSibling.lastElementChild.childNodes[0];
+      const editInput = document.createElement('input');
+      editInput.type = 'text';
+      editInput.className = 'edit-input';
+      article.classList.add('edit-article');
+      const p = document.querySelector('.list-item');
+      editInput.value = editList.textContent;
+      // editList.textContent = editInput.value;
+      const editIndex = e.target.parentElement.dataset.index;
+      editInput.focus();
+      const descDiv = e.target.parentElement.previousElementSibling.lastElementChild;
+      // descDiv.removeChild(editList);
+      editList.style.display = 'none';
+      descDiv.appendChild(editInput);
+
+      editInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+          editList.textContent = editInput.value;
+          // p.textContent = editInput.value
+          descDiv.appendChild(editList);
+          descDiv.removeChild(editInput);
+          p.style.display = 'block';
+          // descDiv.remove('edit-article');
+          article.classList.remove('edit-article');
+          opBtn.classList.remove('hide');
+          removeBtn.classList.remove('show');
+          // console.log(localStorage.tasks)
+        }
+        editLocalStorage(editList.value, status, editIndex);
+      });
+    });
+    removeBtn.addEventListener('click', (e) => {
+      const task = e.target.parentElement.parentElement;
+      const { index } = task.dataset;
+      itemesList.removeChild(task);
+      if (itemesList.children.length === 0) {
+        listContainer.classList.remove('display-container');
+      }
+      removeFromLocalStorage(index);
+      resetIndex();
+      // console.log(localStorage.tasks)
+    });
+
+    itemesList.appendChild(article);
+    listContainer.classList.add('display-container');
+    input.value = '';
+    const itemObject = {};
+    itemObject.desc = item;
+    itemObject.status = false;
+    itemObject.index = index;
+    addToLocalStorage(item, status, index);
+  }
+// console.log(localStorage.tasks)
+};
+// Clear all tasks
+
+myForm.addEventListener('submit', addTask);
+clearbtn.addEventListener('click', () => {
+  const tasks = document.querySelector('.itemesList');
+  while (tasks.firstChild) {
+    tasks.removeChild(tasks.firstChild);
+  }
+  listContainer.classList.remove('display-container');
+  localStorage.removeItem('tasks');
 });
---
+++ src/modules/storage.js
@@ -0,0 +1,37 @@
+const addToLocalStorage = (task, completed, index) => {
+  const tasks = { task, completed, index };
+
+  const tasksList = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+
+  tasksList.push(tasks);
+  localStorage.setItem('tasks', JSON.stringify(tasksList));
+};
+
+const removeFromLocalStorage = (index) => {
+  let tasksList = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+
+  tasksList = tasksList.filter((task) => {
+    if (task.index !== index) {
+      return true;
+    }
+    return false;
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasksList));
+};
+
+const editLocalStorage = (task, status, index) => {
+  let tasksList = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+  tasksList = tasksList.map((item) => {
+    if (item.index === index) {
+      item.task = task;
+      item.completed = status;
+    }
+    return item;
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasksList));
+};
+
+export {
+  addToLocalStorage, removeFromLocalStorage, editLocalStorage,
+};
